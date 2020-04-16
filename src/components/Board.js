13e53062db4774cb242ab7080d3375167d8da77f
cojenco <icathyouyang@gmail.com>
2020-04-15 17:25:46 -0700

    complete wave 1 generateSquareComponents function

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,7 +7,21 @@ import PropTypes from 'prop-types';
 const generateSquareComponents = (squares, onClickCallback) => {
   // Complete this for Wave 1
 
-}
+  const arraySquareComponents = []
+  for(let row = 0; row < squares.length; row++) {
+    for(let col = 0; col < squares[row].length; col++) {
+      arraySquareComponents.push(
+        <Square 
+          id={squares[row][col].id} 
+          value={squares[row][col].value}
+          key={squares[row][col].id}
+        />
+      );
+    }
+  };
+
+  return arraySquareComponents;
+};
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
