a92d0643b110b3c9cc08382d65dfe8a13bf2209b
cojenco <icathyouyang@gmail.com>
2020-04-17 21:10:35 -0700

    clean up code in Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -4,13 +4,10 @@ import PropTypes from 'prop-types';
 import './Square.css'
 
 const Square = (props) => {
-  // For Wave 1 enable this 
-  //  Component to alert a parent 
-  //  component when it's clicked on.
 
   return <button
     className="square"
-  >
+    onClick={() => props.onClickCallback(props.id)}>
     {props.value}
   </button>
 }
