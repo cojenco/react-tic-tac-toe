e9f18a9033d9e0e1e07a40037375775c79f27560
cojenco <icathyouyang@gmail.com>
2020-04-18 20:42:05 -0700

    clean up code and comments

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,45 +5,20 @@ import PropTypes from 'prop-types';
 
 
 const generateSquareComponents = (squares, onClickCallback) => {
-  // Complete this for Wave 1
-
-  // const newArray = squares.map((subarray) {
-  //   for(let j=0; j < subarray.length; j++) {
-  //     console.log(subarray[j]);
-  //     return(
-  //       <Square 
-  //         id={subarray[j].id} 
-  //         value={subarray[j].value}
-  //         key={subarray[j].id}
-  //         onClickCallback={onClickCallback}
-  //       />
-
-  //     );
-  //   }
-  // })
-
-
-
-  const arraySquareComponents = []
-  for(let row = 0; row < squares.length; row++) {
-    for(let col = 0; col < squares[row].length; col++) {
-      arraySquareComponents.push(
-        <Square 
-          id={squares[row][col].id} 
-          value={squares[row][col].value}
-          key={squares[row][col].id}
-          onClickCallback={onClickCallback}
-        />
-      );
-    }
-  };
-
-  return arraySquareComponents;
+  return squares.flat().map((square) => {
+    return(
+      <Square 
+        id={square.id} 
+        value={square.value}
+        key={square.id}
+        onClickCallback={onClickCallback}
+      />
+    );
+  })
 };
 
 const Board = ({ squares, onClickCallback }) => {
   const squareList = generateSquareComponents(squares, onClickCallback);
-  console.log(squareList);
   return <div className="grid" >
     {squareList}
   </div>
